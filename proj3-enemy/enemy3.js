332df405fba9ff8ce286713a71f016150df5cbe1
Alexander_Ovchar <azalexxx02@gmail.com>
2024-03-30 20:30:47 +0200

    add enemy type 3

--- proj3-enemy/enemy3.js
+++ proj3-enemy/enemy3.js
@@ -12,26 +12,33 @@ let gameFrame = 0;
 class Enemy {
   constructor() {
     this.image = new Image();
-    this.image.src = "./enemies/enemy1.png";
-    // this.speed = Math.random() * 4 - 2;
-    this.spriteWidth = 293;
-    this.spriteHeight = 155;
+    this.image.src = "./enemies/enemy3.png";
+    this.speed = Math.random() * 4 + 1;
+    this.spriteWidth = 218;
+    this.spriteHeight = 177;
     this.width = this.spriteWidth / 2.5;
     this.height = this.spriteHeight / 2.5;
     this.x = Math.random() * (CANVAS_WIDTH - this.width);
     this.y = Math.random() * (CANVAS_HEIGHT - this.height);
     this.frame = 0;
-    this.flapSpeed = Math.floor(Math.random() * 3) + 1;
+    this.flapSpeed = Math.floor(Math.random() * 4) + 1;
+    this.angle = Math.random() * 360;
+    this.angleSpeed = Math.random() * 1;
+    // this.curve = Math.random() * 200 + 50;
   }
   update() {
-    this.x += Math.random() * 15 - 7.5;
-    this.y += Math.random() * 10 - 5;
+    this.x = (CANVAS_WIDTH / 2) * Math.sin(this.angle * (Math.PI / 100)) + (CANVAS_WIDTH / 2 - this.width / 2);
+    if (this.x + this.width < 0) {
+      this.x = CANVAS_WIDTH;
+    }
+    // this.y += this.curve * Math.sin(this.angle);
+    this.y = (CANVAS_HEIGHT / 2) * Math.cos(this.angle * (Math.PI / 270)) + (CANVAS_HEIGHT / 2 - this.height / 2);
+    this.angle += this.angleSpeed;
     if (gameFrame % this.flapSpeed === 0) {
       this.frame > 4 ? (this.frame = 0) : this.frame++;
     }
   }
   draw() {
-    // ctx.strokeRect(this.x, this.y, this.width, this.height);
     ctx.drawImage(
       this.image,
       this.frame * this.spriteWidth,
@@ -46,7 +53,6 @@ class Enemy {
   }
 }
 
-// const enemy1 = new Enemy();
 for (let i = 0; i < numberOfEnemies; i++) {
   enemiesArray.push(new Enemy());
 }
