8b32e6448bdd25d6f43f1538c72d2093317a3284
Alexander_Ovchar <azalexxx02@gmail.com>
2024-03-30 23:54:40 +0200

    add boom effects

--- proj4-collision_detection/index.js
+++ proj4-collision_detection/index.js
@@ -1,16 +1,71 @@
+const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = (canvas.width = 500);
 const CANVAS_HEIGHT = (canvas.height = 700);
-const numberOfEnemies = 10;
-const enemiesArray = [];
+let canvasPosition = canvas.getBoundingClientRect();
 
 const explosions = [];
 
 class Explosion {
   constructor(x, y) {
-    this.x = x;
-    this.y = y;
     this.spriteWidth = 200;
     this.spriteHeight = 179;
+    this.width = this.spriteWidth * 0.7;
+    this.height = this.spriteHeight * 0.7;
+    this.x = x - this.width / 2;
+    this.y = y - this.height / 2;
+    this.image = new Image();
+    this.image.src = "./boom.png";
+    this.frame = 0;
+    this.timer = 0;
+    this.sound = new Audio();
+    this.sound.src = "./sounds/Fire impact 1.wav";
+  }
+  update() {
+    if (this.frame === 0) {
+      this.sound.play();
+    }
+    this.timer++;
+    if (this.timer % 10 === 0) {
+      this.frame++;
+    }
+  }
+  draw() {
+    ctx.drawImage(
+      this.image,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
+
+window.addEventListener("click", function (e) {
+  createAnimation(e);
+});
+
+function createAnimation(e) {
+  let positionX = e.x - canvasPosition.left;
+  let positionY = e.y - canvasPosition.top;
+  explosions.push(new Explosion(positionX, positionY));
+}
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  for (let i = 0; i < explosions.length; i++) {
+    explosions[i].update();
+    explosions[i].draw();
+    if (explosions[i].frame > 5) {
+      explosions.splice(i, 1);
+      i--;
+    }
+  }
+  requestAnimationFrame(animate);
+}
+
+animate();
