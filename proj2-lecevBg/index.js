2614cbbcbd6709ae3b647bed00ebe13511d3ee5d
Alexander_Ovchar <azalexxx02@gmail.com>
2024-03-30 16:23:29 +0200

    complete level parallax background

--- proj2-lecevBg/index.js
+++ proj2-lecevBg/index.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = (canvas.width = 800);
 const CANVAS_HEIGHT = (canvas.height = 700);
 
-const gameSpeed = 15;
+const gameSpeed = 10;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = "./backgroundLayers/layer-1.png";
