435d807c46a415da5bbb47cb69cd6aea1df54e1b
Alexander_Ovchar <azalexxx02@gmail.com>
2024-04-18 19:29:33 +0300

    add flying enemies

--- proj9-finaly_game/index.js
+++ proj9-finaly_game/index.js
@@ -1,6 +1,7 @@
 import Player from "./player.js";
 import InputHandler from "./input.js";
 import { Background } from "./background.js";
+import { FlyingEnemy, GroundEnemy, ClimbingEnemy } from "./enemies.js";
 
 window.addEventListener("load", function () {
   const canvas = document.getElementById("canvas");
@@ -18,15 +19,37 @@ window.addEventListener("load", function () {
       this.background = new Background(this);
       this.player = new Player(this);
       this.input = new InputHandler();
+      this.enemies = [];
+      this.enemyTimer = 0;
+      this.enemyInterval = 1000;
     }
 
     update(deltaTime) {
       this.background.update();
       this.player.update(this.input.keys, deltaTime);
+      if (this.enemyTimer > this.enemyInterval) {
+        this.addEnemy();
+        this.enemyTimer = 0;
+      } else {
+        this.enemyTimer += deltaTime;
+      }
+      this.enemies.forEach((enemy) => {
+        enemy.update(deltaTime);
+      });
     }
     draw(context) {
       this.background.draw(context);
       this.player.draw(context);
+      this.enemies.forEach((enemy) => {
+        enemy.draw(context);
+        if (enemy.markerForDeletion) {
+          this.enemies.splice(this.enemies.indexOf(enemy), 1);
+        }
+      });
+    }
+    addEnemy() {
+      this.enemies.push(new FlyingEnemy(this));
+      console.log(this.enemies);
     }
   }
 
