d47114b3e1b9b9e52c52774e690a16231134893b
Alexander_Ovchar <azalexxx02@gmail.com>
2024-04-18 22:45:30 +0300

    add Rolling state

--- proj9-finaly_game/input.js
+++ proj9-finaly_game/input.js
@@ -13,16 +13,19 @@ export default class InputHandler {
       ) {
         this.keys.push(event.key);
       } else if (event.key === "d") this.game.debug = !this.game.debug;
+      console.log(this.keys);
     });
     window.addEventListener("keyup", (event) => {
       if (
         event.key === "ArrowDown" ||
         event.key === "ArrowUp" ||
         event.key === "ArrowLeft" ||
-        event.key === "ArrowRight"
+        event.key === "ArrowRight" ||
+        event.key === "Enter"
       ) {
         this.keys.splice(this.keys.indexOf(event.key), 1);
       }
+      console.log(this.keys);
     });
   }
 }
