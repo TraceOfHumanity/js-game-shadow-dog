b5d9d4e31bd4123bcd9a7648d1a6ed3f7bc597a9
Alexander_Ovchar <azalexxx02@gmail.com>
2024-04-19 18:24:46 +0300

    complete game

--- proj9-finaly_game/index.js
+++ proj9-finaly_game/index.js
@@ -56,10 +56,9 @@ window.addEventListener("load", function () {
         particle.update();
         if (particle.markedForDeletion) this.particles.splice(index, 1);
       });
-      if (this.particles.length > this.maxParticles) this.particles = this.particles.slice(0, this.maxParticles);
+      if (this.particles.length > this.maxParticles) this.particles.length = this.maxParticles;
       this.collisions.forEach((collision, index) => {
         collision.update(deltaTime);
-
         if (collision.markedForDeletion) this.collisions.splice(index, 1);
       });
     }
